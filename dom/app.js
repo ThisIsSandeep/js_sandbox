4cb9cd837578f706721d641b6952d56064033b52
ThisIsSandeep <thesandybansal@gmail.com>
2018-09-16 00:01:54 +0530

    Examining the DOM object

---
+++ dom/app.js
@@ -0,0 +1,17 @@
+let val;
+
+val = document; // It will give whole document
+val = document.all;
+val = document.all[0];
+val = document.all.length;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+// We can select things without select but it's not recommended
+val = document.title;
+
+console.log(val);
